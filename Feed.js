9bf3a396749917bf9b81bebd92deda7a5647c9b1
Athman Gude <athmangude@gmail.com>
2015-11-02 13:05:41 +0300

    Getting started with fecthing data from Github

--- Feed.js
+++ Feed.js
@@ -11,6 +11,8 @@ var {
   ListView
 } = React;
 
+// var AuthenticationService = require('./AuthenticationService');
+
 class Feed extends Component {
   constructor(props){
     super(props);
@@ -22,6 +24,31 @@ class Feed extends Component {
     this.state = {
       dataSource: dataSource.cloneWithRows(['A', 'B', 'C'])
     };
+  }
+
+  componentDidMount() {
+    this.fetchFeed();
+  }
+
+  fetchFeed() {
+    require('./AuthenticationService').getAuthenticationInfo((error, authenticationInfo) => {
+
+      var url = 'https://api.github.com/users/'+authenticationInfo.user.login+'/received_events';
+
+      fetch(url, {
+        headers: authenticationInfo.header
+      }).then((response) => {
+        console.log(response);
+        response.json
+      }).then((responseData) => {
+        console.log(responseData);
+        var feedItems = responseData.filter((ev) => ev.type == 'PushEvent');
+        console.log(feedItems);
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(feedItems)
+        });
+      });
+    });
   }
 
   renderRow(rowData) {
